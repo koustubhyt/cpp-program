982266e026a0b06f171676ba067d464c1adde691
koustubhyt <koustubh.phadnis2020@gmail.com>
2025-08-04 18:38:18 +0530

    03/08

--- 12simpleinterest.cpp
+++ 12simpleinterest.cpp
@@ -1,11 +1,15 @@
 #include<iostream>
 using namespace std;
-int main()
+int main ()
 {
-    int p,r,t;
+    int p,r,t,simple_interest;
     cout<<"Enter the value of p : ";
     cin>>p;
-    
-
+    cout<<"Enter the value of r : ";
+    cin>>r;
+    cout<<"Enter the value of t : ";
+    cin>>t;
+    simple_interest=(p*r*t)/100;
+    cout<<"simple interest = "<<simple_interest;
 
 }
