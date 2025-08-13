10e258db42051ef919abeb029f349c29885e4929
koustubhyt <koustubh.phadnis2020@gmail.com>
2025-08-13 19:06:49 +0530

    13/08

---
+++ 19covertindianrstodollar.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main ()
+{
+    float rs,dollar;
+    cout<<"Enter the value of rupees : ";
+    cin>>rs;
+     dollar=rs/87.4729;
+    cout<<"value of rs : "<<rs<<" into "<<"value of dollar : "<<dollar<<endl;
+}
